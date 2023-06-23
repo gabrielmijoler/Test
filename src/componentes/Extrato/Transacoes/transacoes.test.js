7647d0adf6082dd6087b4df67e0cce70b3c6ea66
GABRIEL S MIJOLER <69941946+gabrielmijoler@users.noreply.github.com>
2023-06-23 18:00:12 -0300

    feat: ultimos testes

--- src/componentes/Extrato/Transacoes/transacoes.test.js
+++ src/componentes/Extrato/Transacoes/transacoes.test.js
@@ -14,3 +14,16 @@ test("Deve renderizar o mesmo componentes com props atualizadas",() => {
   expect(tipotransacao).toHaveTextContent('Depósito');
   expect(valortransacao).toHaveTextContent('R$ 100');
 });
+
+test("Verificar transação",() => {
+  const transacao ={
+      transacao:'Transferência',
+      valor: 100,
+    }; 
+
+  render(<Transacoes estilos={estilos} transacao={transacao}/>);
+  const tipotransacao = screen.getByTestId('tipoTransacao');
+  const valortransacao = screen.getByTestId('valorTransacao');
+  expect(tipotransacao).toHaveTextContent('Transferência');
+  expect(valortransacao).toHaveTextContent('R$ 100');
+});
--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -1,4 +1,5 @@
 const { render, screen } = require("@testing-library/react");
+import userEvent from '@testing-library/user-event';
 import Formulario from './index'
 
 
@@ -21,3 +22,26 @@ test("Pode ser preenchido",()=>{
     expect(campotexto).toHaveAttribute('type', 'number');
 });
 })
+
+test("Deve chamar um evento de onSubmit ao clicar em realizar transação",()=>{
+    const realizarTransacao = jest.fn(); // dublando, mocando
+
+    render(<Formulario realizarTransacao={realizarTransacao}/>);
+    const botao = screen.getByRole('button');
+    userEvent.click(botao)
+    expect(realizarTransacao).toHaveBeenCalledTimes(1);
+});
+
+test('Deve ser possível selecionar uma opção do elemento <select/>', () => {
+    render(<Formulario />); // renderiza o componente
+    const select = screen.getByRole('combobox'); // faz a consulta do elemento select
+    userEvent.selectOptions(select, ['Depósito']); // simula a ação de selecionar uma opção do select
+
+    expect(
+      screen.getByRole('option', { name: 'Selecione um tipo de transação' })
+        .selected
+    ).toBe(false); // verifica se a opção de selecionar um tipo de transação não foi selecionada
+    expect(screen.getByRole('option', { name: 'Depósito' }).selected).toBe(
+      true
+    ); // verifica se a opção de depósito foi selecionada
+  });
